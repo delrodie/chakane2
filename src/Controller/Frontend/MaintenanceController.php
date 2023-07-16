469e6b39550e0739813291ee6720e1c83b85bcce
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-07-16 10:59:34 +0000

    Frontend Maintenance

---
+++ src/Controller/Frontend/MaintenanceController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Frontend;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/maintenance')]
+class MaintenanceController extends AbstractController
+{
+    #[Route('/', name: 'app_frontend_maintenance_index')]
+    public function index()
+    {
+        return $this->render('frontend/maintenance.html.twig');
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MaintenanceRepository;
 use App\Service\UserActivityLogger;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -10,10 +11,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private MaintenanceRepository $maintenanceRepository
+    )
+    {
+    }
 
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        if ($this->maintenanceRepository->findOneBy(['statut' => true]))
+            return $this->redirectToRoute('app_frontend_maintenance_index',[],Response::HTTP_SEE_OTHER);
+
         return $this->render('frontend/home.html.twig');
     }
 }
