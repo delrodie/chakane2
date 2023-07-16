1d302b6d92db0ebb9e35226aa8419d39027335f0
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-07-16 09:25:28 +0000

    Backend Maintenance

---
+++ src/Controller/BackendMaintenanceController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Maintenance;
+use App\Form\MaintenanceType;
+use App\Repository\MaintenanceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/backend/maintenance')]
+class BackendMaintenanceController extends AbstractController
+{
+    #[Route('/', name: 'app_backend_maintenance_index', methods: ['GET'])]
+    public function index(MaintenanceRepository $maintenanceRepository): Response
+    {
+        $maintenance = $maintenanceRepository->findOneBy([],['id'=>'DESC']);
+        if ($maintenance)
+            return $this->redirectToRoute('app_backend_maintenance_edit',['id'=>$maintenance->getId()], Response::HTTP_SEE_OTHER);
+
+        return $this->redirectToRoute('app_backend_maintenance_new',[],Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/new', name: 'app_backend_maintenance_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $maintenance = new Maintenance();
+        $form = $this->createForm(MaintenanceType::class, $maintenance);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($maintenance);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_backend_maintenance_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('backend_maintenance/new.html.twig', [
+            'maintenance' => $maintenance,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_backend_maintenance_show', methods: ['GET'])]
+    public function show(Maintenance $maintenance): Response
+    {
+        return $this->render('backend_maintenance/show.html.twig', [
+            'maintenance' => $maintenance,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_backend_maintenance_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Maintenance $maintenance, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(MaintenanceType::class, $maintenance);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_backend_maintenance_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('backend_maintenance/edit.html.twig', [
+            'maintenance' => $maintenance,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_backend_maintenance_delete', methods: ['POST'])]
+    public function delete(Request $request, Maintenance $maintenance, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$maintenance->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($maintenance);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_backend_maintenance_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
